8eedab8934454cfe8ace5666c3ac4035867172ea
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-02-26 18:38:53 +0100

    home + menu + api

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sir-tracker
+
 A simple tracking interface
+
+## webapp
+
+Live [here](https://sir-tracker.web.app/)
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
